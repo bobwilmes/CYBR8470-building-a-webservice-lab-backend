da47f2961b3b475483cbb7431b079b9ce7db9a8e
Bob Wilmes <bobwilmes@gmail.com>
2021-10-15 20:26:42 -0500

    added endpoints for events and IFTTT

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,14 @@ from django.views.decorators.csrf import csrf_exempt
 #REST API routes
 router = routers.DefaultRouter(trailing_slash=False)
 
+#urlpatterns = [
+#  url(r'^session', csrf_exempt(controllers.Session.as_view())),
+#  url(r'^register', csrf_exempt(controllers.Register.as_view())),
+#  url(r'^events', csrf_exempt(controllers.Events.as_view())),
+#  url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
+#  url(r'^', include(router.urls)),
+# ]
+
 urlpatterns = [
     url(r'^session', csrf_exempt(controllers.Session.as_view())),
     url(r'^register', csrf_exempt(controllers.Register.as_view())),
